8cc82b94f1c1305266212b4086b2e52922aa38c8
sha-zha <adeline.servan@gmail.com>
2023-09-16 21:19:15 +0100

    show user

--- controllers/api/v1/UserController.js
+++ controllers/api/v1/UserController.js
@@ -2,15 +2,47 @@ const {User} = require('../../../src/models/index');
 
 let controller = {}
 
+controller.create = async (req,res) =>{
+
+};
+
 controller.index = async (req,res) =>{
 	try {
 		const data = await User.findAll({
     		attributes: {exclude: ['password']},
     		order: [['id','DESC']]});
 
+		return res.status(200).json({
+      		data
+    	});
+
+	}catch(e){
+		
+		 return res.status(500).send('Error!: ');
+	}
+};
+
+controller.show = async (req,res) =>{
+	let id = req.body.id;
+
+	try {
+		const data = await User.findByPk(id);
+		
+		return res.status(200).json({
+      		data
+    	});
+		
 	}catch(e){
 		 return res.status(500).send('Error!');
 	}
+};
+
+controller.update = async (req,res) =>{
+
+};
+
+controller.deleted = async (req,res) =>{
+
 };
 
 module.exports = controller;
--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,10 @@ const UserController = require('../controllers/api/v1/UserController');
 
 /* CRUD USERS */
 router.get('/users',UserController.index);
+router.get('/users/:id',UserController.show);
+router.post('/users/:id',UserController.update);
+router.post('/users/add',UserController.create);
+router.post('/users/deleted/:id',UserController.deleted);
 // CRUD CHECKPOINTS
 
 // REFERENTIALS
